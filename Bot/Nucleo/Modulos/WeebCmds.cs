d5ae843011e61e846db1d4b8d8772bf6f657fe20
Takasakiii <lucasmc2709@live.com>
2019-02-24 19:32:41 -0300

    Update WeebCmds.cs

--- Bot/Nucleo/Modulos/WeebCmds.cs
+++ Bot/Nucleo/Modulos/WeebCmds.cs
@@ -21,12 +21,13 @@ namespace Bot.Nucleo.Modulos
 
         public void getImg()
         {
-
+            //imagine um codigo pika aki
+            //foda esse codigo kkkkkkkkkk esse aki so zuando
         }
 
         public async Task Hug(WeebClient weebClient)
         {
-            var img = await weebClient.GetRandomAsync("hug", new string[] { }, FileType.Gif, false, NsfwSearch.False);
+            var img = await weebClient.GetRandomAsync("hug", new string[] { }, FileType.Gif, false, NsfwSearch.False); //ja disse hj evite var???
 
             EmbedBuilder builder = new EmbedBuilder()
                 .WithImageUrl(img.Url)
@@ -43,7 +44,7 @@ namespace Bot.Nucleo.Modulos
                 if (comando[1] == "t" || comando[1] == "tipos")
                 {
                     Weeb.net.Data.TypesData tipos = await weebClient.GetTypesAsync();
-                    string[] tiposArr = tipos.Types.ToArray();
+                    string[] tiposArr = tipos.Types.ToArray(); //enumetates n devem virar array (erro de otimização grave)
                     string txt = "";
 
                     for (int i = 0; i < tiposArr.Length; i++)
@@ -63,7 +64,7 @@ namespace Bot.Nucleo.Modulos
                 {
                     try
                     {
-                        var img = await weebClient.GetRandomAsync(comando[2], new string[] { }, FileType.Gif, false, NsfwSearch.False);
+                        var img = await weebClient.GetRandomAsync(comando[2], new string[] { }, FileType.Gif, false, NsfwSearch.False); //entao n preciso nem comentar ne??
 
                         EmbedBuilder builder = new EmbedBuilder()
                             .WithTitle(img.BaseType)
@@ -78,7 +79,7 @@ namespace Bot.Nucleo.Modulos
                     {
                         await context.SendErrorAsync($"você não me disse o tipo de gif ou esse tipo não existe");
                     }
-                }
+                } // default case nesse caso eh bem interessante XD
             } catch {
                 EmbedBuilder builder = new EmbedBuilder()
                     .WithTitle($"{context.User}")
@@ -86,7 +87,7 @@ namespace Bot.Nucleo.Modulos
                     .WithErrorColor();
                 Embed embed = builder.Build();
 
-                await context.Channel.SendMessageAsync("", embed: embed).ConfigureAwait(false);
+                await context.Channel.SendMessageAsync("", embed: embed).ConfigureAwait(false); 
             }
         }
     }
