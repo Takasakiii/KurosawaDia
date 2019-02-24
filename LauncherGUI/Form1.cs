2b8199434766a6607e106587055021e192b8271d
Takasakiii <lucasmc2709@live.com>
2019-02-24 20:00:49 -0300

    Update Form1.cs

--- LauncherGUI/Form1.cs
+++ LauncherGUI/Form1.cs
@@ -16,7 +16,7 @@ namespace LauncherGUI
     public partial class frGUI : Form
     {
         private Thread t;
-        Core core = new Core();
+        Core core = new Core(); // n eh um objeto geral de todos os metodos
 
         public frGUI()
         {
