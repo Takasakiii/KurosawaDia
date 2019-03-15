02d6497c7969a3cb6264c78c9102bdaf7284f191
Yummi <YummiSenpai@outlook.com>
2019-03-15 18:01:04 -0300

    Adicionado suporte a linux XD

---
+++ NetCoreGUI/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using Bot;
+using Bot.Singletons;
+using System;
+using System.Threading;
+
+namespace NetCoreGUI
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.Write("Digite o local da DB: ");
+                SingletonConfig.localConfig = Console.ReadLine();
+                new Thread(() => new Core().IniciarBot()).Start();
+                Console.Write("\nO Bot foi iniciado");
+            } else
+            {
+                SingletonConfig.localConfig = args[0];
+                new Thread(() => new Core().IniciarBot()).Start();
+                Console.Write("\nO Bot foi iniciado");
+            }
+        }
+    }
+}
