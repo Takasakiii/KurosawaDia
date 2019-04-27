480a7dd96806827e385e9417115eb33dc4dad365
Yummi <YummiSenpai@outlook.com>
2019-04-27 02:28:41 -0300

    refiz os cmds nsfw para que tenha uma maior aletoriedade (aquela carinha aqui)

--- Bot/Comandos/Nsfw.cs
+++ Bot/Comandos/Nsfw.cs
@@ -7,7 +7,7 @@ namespace Bot.Comandos
 {
     public class Nsfw : Weeb
     {
-        private void nsfw(CommandContext context, string url, string get = "message", int quantidade = 1)
+        private void nsfw(CommandContext context, Link[] links, int quantidade = 1)
         {
             ITextChannel canal = context.Channel as ITextChannel;
             if (context.IsPrivate || canal.IsNsfw)
@@ -16,8 +16,11 @@ namespace Bot.Comandos
 
                 if (quantidade <= 1)
                 {
+                    Random rand = new Random();
+                    int i = rand.Next(links.Length);
+
                     context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                            .WithImageUrl(http.GetSite(url, get))
+                            .WithImageUrl(http.GetSite(links[i].url, links[i].tipo))
                             .WithColor(Color.DarkPurple)
                         .Build());
                 }
@@ -26,8 +29,12 @@ namespace Bot.Comandos
                     string txt = "";
                     for (int i = 0; i < quantidade; i++)
                     {
-                        txt += $"{http.GetSite(url, get)}\n";
+                        Random rand = new Random();
+                        int x = rand.Next(links.Length);
+
+                        txt += $"{http.GetSite(links[x].url, links[x].tipo)}\n";
                     }
+
                     context.Channel.SendMessageAsync(txt);
                 }
             }
@@ -41,43 +48,55 @@ namespace Bot.Comandos
 
         public void hentai(CommandContext context, object[] args)
         {
-            nsfw(context, "https://nekobot.xyz/api/image?type=hentai");
+            Link[] links = {
+                new Link("https://nekobot.xyz/api/image?type=hentai", "message"),
+                new Link("https://nekos.life/api/v2/img/nsfw_neko_gif", "url"),
+                new Link("https://nekos.life/api/v2/img/lewdk", "url"),
+            };
+
+            nsfw(context, links);
         }
 
         public void hentaibomb(CommandContext context, object[] args)
         {
 
-            Links[] links = {
-                new Links("https://nekobot.xyz/api/image?type=hentai", "message"),
-                new Links("https://nekos.life/api/v2/img/nsfw_neko_gif", "url"),
-                new Links("https://nekos.life/api/v2/img/lewdk", "url"),
+            Link[] links = {
+                new Link("https://nekobot.xyz/api/image?type=hentai", "message"),
+                new Link("https://nekos.life/api/v2/img/nsfw_neko_gif", "url"),
+                new Link("https://nekos.life/api/v2/img/lewdk", "url"),
             };
 
-            Random rand = new Random();
-            int i = rand.Next(links.Length);
-
-            nsfw(context, links[i].url, links[i].tipo, 5);
+            nsfw(context, links, 5);
         }
 
         public void hneko(CommandContext context, object[] args)
         {
-            string[] links = { "https://nekos.life/api/v2/img/nsfw_neko_gif", "https://nekos.life/api/v2/img/lewdk" };
-            nsfw(context, new ArrayExtensions().GetRandom(links), "url");
+            Link[] links = {
+                new Link("https://nekos.life/api/v2/img/lewdki", "url"),
+                new Link("https://nekos.life/api/v2/img/nsfw_neko_gif", "url"),
+            };
+
+            nsfw(context, links);
         }
 
         public void anal(CommandContext context, object[] args)
         {
-            nsfw(context, "https://nekobot.xyz/api/image?type=anal", "message");
+            Link[] links = {
+                new Link( "https://nekobot.xyz/api/image?type=anal", "message"),
+            };
+
+            nsfw(context, links);
+
         }
     }
 
 
-    class Links
+    class Link
     {
         public string url { private set; get; }
         public string tipo { private set; get; }
 
-        public Links(string url, string tipo)
+        public Link(string url, string tipo)
         {
             this.url = url;
             this.tipo = tipo;
