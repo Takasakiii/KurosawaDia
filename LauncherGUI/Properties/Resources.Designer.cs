5779472ae2f5715b27c726a6aa92f0e797a8991c
Lucas Mendes Campos <lucasmc2709@live.com>
2019-07-10 06:14:10 -0300

    Terminado o designe base da tab de Inicialização

--- LauncherGUI/Properties/Resources.Designer.cs
+++ LauncherGUI/Properties/Resources.Designer.cs
@@ -74,6 +74,16 @@ namespace Bot.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Shiro {
+            get {
+                object obj = ResourceManager.GetObject("Shiro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap universe {
             get {
                 object obj = ResourceManager.GetObject("universe", resourceCulture);
