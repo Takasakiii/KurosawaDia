07459f44d57bfdc7ca4bf67dff2a1f0deb83b40d
Lucas Mendes Campos <lucasmc2709@live.com>
2019-07-10 07:42:49 -0300

    Criado a base dos modelos para serem aplicados na tela de configuração

---
+++ ConfigurationControler/Modelos/Status.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationControler.Modelos
+{
+    public class Status
+    {
+        //Tipos de Dados:
+        public enum TiposDeStatus { Jogando, Live, Ouvindo, Assistindo }
+
+        //Atributos
+        public uint id { private set; get; }
+        public string status_jogo { private set; get; }
+        public TiposDeStatus status_tipo { private set; get; }
+        
+        public Status(string status_jogo, TiposDeStatus status_tipo)
+        {
+            this.status_jogo = status_jogo;
+            this.status_tipo = status_tipo;
+        }
+
+    }
+
+}
---
+++ ConfigurationControler/Singletons/DB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationControler.Singletons
+{
+    public static class DB
+    {
+        public static string LocalFile { private set; get; }
+        public static bool ExisteFile { private set; get; }
+
+        public static void SetDB (bool _ExisteFile, string _LocalFile)
+        {
+            ExisteFile = _ExisteFile;
+            LocalFile = _LocalFile;
+        }
+    }
+}
