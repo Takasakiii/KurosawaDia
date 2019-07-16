a709c4d4c71a9c2e6b68279003e349937ca09438
Lucas Mendes Campos <lucasmc2709@live.com>
2019-07-15 23:24:43 -0300

    Correção pq quem criou tava drogado

--- ConfigurationControler/Singletons/DB.cs
+++ ConfigurationControler/Singletons/DB.cs
@@ -7,7 +7,7 @@
                 "CREATE TABLE \"DiaConfig\" (\"id\" INTEGER PRIMARY KEY,\"token\" TEXT NOT NULL,\"prefix\" TEXT,\"idDono\" INTEGER NOT NULL)",
                 "CREATE TABLE \"DbConfig\" (\"id\"    INTEGER PRIMARY KEY,\"ip\"    TEXT NOT NULL,\"database\"  TEXT NOT NULL,\"login\" TEXT NOT NULL,\"senha\" TEXT NOT NULL)",
                 "CREATE TABLE \"Status\" (\"status_id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\"status_jogo\" TEXT NOT NULL, \"status_url\" TEXT, \"status_tipo\" INTEGER NOT NULL)",
-                "CREATE TABLE \"Linguagens\" (\"idString\"  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \"idiomaString\"  TEXT NOT NULL, \"stringNumber\"  INTEGER NOT NULL, \"String\" INTEGER NOT NULL)"};
+                "CREATE TABLE \"Linguagens\" (\"idString\"  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \"idiomaString\"  TEXT NOT NULL, \"stringIdentifier\"  TEXT NOT NULL, \"String\" INTEGER NOT NULL)"};
 
         public const string localDB = "configDia.db";
 
