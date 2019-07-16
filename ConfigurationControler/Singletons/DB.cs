44509b51ed32ac0b7fc0b6741763cd50f3bd680d
Lucas Mendes Campos <lucasmc2709@live.com>
2019-07-15 21:23:16 -0300

    Criado a nova Tabela que armazenara as linguagens e string do bot

--- ConfigurationControler/Singletons/DB.cs
+++ ConfigurationControler/Singletons/DB.cs
@@ -6,7 +6,8 @@
         public readonly static string[] sqlCriacao = { "CREATE TABLE \"ApiConfig\" (\"id\"INTEGER PRIMARY KEY,\"WeebToken\" TEXT NOT NULL)",
                 "CREATE TABLE \"DiaConfig\" (\"id\" INTEGER PRIMARY KEY,\"token\" TEXT NOT NULL,\"prefix\" TEXT,\"idDono\" INTEGER NOT NULL)",
                 "CREATE TABLE \"DbConfig\" (\"id\"    INTEGER PRIMARY KEY,\"ip\"    TEXT NOT NULL,\"database\"  TEXT NOT NULL,\"login\" TEXT NOT NULL,\"senha\" TEXT NOT NULL)",
-                "CREATE TABLE \"Status\" (\"status_id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\"status_jogo\" TEXT NOT NULL, \"status_url\" TEXT, \"status_tipo\" INTEGER NOT NULL)" };
+                "CREATE TABLE \"Status\" (\"status_id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\"status_jogo\" TEXT NOT NULL, \"status_url\" TEXT, \"status_tipo\" INTEGER NOT NULL)",
+                "CREATE TABLE \"Linguagens\" (\"idString\"  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \"idiomaString\"  TEXT NOT NULL, \"stringNumber\"  INTEGER NOT NULL, \"String\" INTEGER NOT NULL)"};
 
         public const string localDB = "configDia.db";
 
