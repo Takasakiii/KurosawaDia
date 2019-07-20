243d8f49fe2c9799795f1a0a9e83cd7b0294d7a4
Lucas Mendes Campos <lucasmc2709@live.com>
2019-07-19 22:12:08 -0300

    Adionado o mostrar as tetas db comandinho lindo kkkkk

--- DBs/Ayura.sql
+++ DBs/Ayura.sql
@@ -155,6 +155,11 @@ create procedure AdcAjudanteIdol (
 	end if;
 end$$
 
+create procedure MostrarAsTetas()
+begin
+	select Servidores.nome_servidor, Usuarios.nome_usuario from Servidores_Usuarios join Servidores on Servidores.codigo_servidor = Servidores_Usuarios.Servidores_codigo_servidor join Usuarios on Usuarios.codigo_usuario = Servidores_Usuarios.Usuarios_codigo_usuario;
+end$$
+
 delimiter ;
 call atualizarPrefix(556580866198077451, "!");
 
