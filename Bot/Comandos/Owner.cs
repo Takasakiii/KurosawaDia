2e3a31c1a1a6d9fbcfdb97b7f6967fedf2530c33
Yummi <YummiSenpai@outlook.com>
2019-07-29 20:22:27 -0300

    bem ta quase acabado o send e foi isso por hj crianças

--- Bot/Comandos/Owner.cs
+++ Bot/Comandos/Owner.cs
@@ -5,6 +5,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
+using System.Linq;
 using static Bot.DataBase.MainDB.Modelos.Adms;
 using static Bot.DataBase.MainDB.Modelos.Servidores;
 
@@ -67,6 +68,62 @@ namespace Bot.Comandos
                             .WithDescription(StringCatch.GetString("setEspecialErro", "Meu caro vc n digitou o cmd do jeito certo"))
                             .WithColor(Color.DarkPurple)
                          .Build());
+                    }
+                }
+            }
+        }
+
+        public void send(CommandContext context, object[] args)
+        {
+            Usuarios usuario = new Usuarios(context.User.Id, context.User.Username);
+            Adms adm = new Adms(usuario);
+
+            if (new AdmsDAO().GetAdm(ref adm))
+            {
+                if (adm.permissoes == PermissoesAdm.Donas)
+                {
+                    string[] comando = (string[])args[1];
+                    string msg = string.Join(" ", comando, 1, (comando.Length - 1));
+                    string[] id_msg = msg.Split('|');
+
+                    if(id_msg[0].StartsWith("c "))
+                    {
+                        IChannel canal = context.Client.GetChannelAsync(Convert.ToUInt64(comando[2])).GetAwaiter().GetResult();
+
+                        try
+                        {
+                            var embedo = new EmbedControl().CriarEmbedJson(id_msg[1]);
+                            (canal as IMessageChannel).SendMessageAsync(embedo.Item1, embed: embedo.Item2);
+                        }
+                        catch
+                        {
+                            (canal as IMessageChannel).SendMessageAsync(id_msg[1]);
+                        }               
+                        
+                    } else if (id_msg[0].StartsWith("s "))
+                    {
+                        IGuild servidor = context.Client.GetGuildAsync(Convert.ToUInt64(comando[2])).GetAwaiter().GetResult();
+                        System.Collections.Generic.List<IGuildChannel> canais = servidor.GetChannelsAsync().GetAwaiter().GetResult().ToList();
+                        bool parar = false;
+                        for(int i =0; i < canais.Count && !parar; i++)
+                        {
+                            ChannelPermissions permissoes = (context.User as IGuildUser).GetPermissions(canais[i]);
+                            if (permissoes.SendMessages)
+                            {
+                                try
+                                {
+                                    var embedo = new EmbedControl().CriarEmbedJson(id_msg[1]);
+                                    (canais[i] as IMessageChannel).SendMessageAsync(embedo.Item1, embed: embedo.Item2);
+                                }
+                                catch
+                                {
+                                    (canais[i] as IMessageChannel).SendMessageAsync(id_msg[1]);
+                                }
+                                parar = true;
+                            }
+                        }
+                        
+                        
                     }
                 }
             }
