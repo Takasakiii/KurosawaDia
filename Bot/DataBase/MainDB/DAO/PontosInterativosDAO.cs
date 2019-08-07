5e378dd40f52e5e98a27d969a9d436dde211bff1
Lucas Mendes Campos <lucasmc2709@live.com>
2019-08-07 17:51:53 -0300

    DAO responsavel por cuidar do subevent do pontos interacao e atualização do msm

---
+++ Bot/DataBase/MainDB/DAO/PontosInterativosDAO.cs
@@ -0,0 +1,40 @@
+﻿using Bot.DataBase.Constructors;
+using Bot.DataBase.MainDB.Modelos;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Bot.DataBase.MainDB.Modelos.ConfiguracoesServidor;
+
+namespace Bot.DataBase.MainDB.DAO
+{
+    public class PontosInterativosDAO
+    {
+        private MySqlConnection conexao = new MySqlConstructor().Conectar();
+
+        public bool AdicionarPonto (PontosInterativos pontosInterativos, ref PI piSaida)
+        {
+            bool retorno = false;
+            const string sql = "call AddPI(@servidor, @usuario)";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@servidor", pontosInterativos.servidores_usuarios.servidor.id);
+            cmd.Parameters.AddWithValue("@usuario", pontosInterativos.servidores_usuarios.usuario.id);
+
+            MySqlDataReader rs = cmd.ExecuteReader();
+            if (rs.Read())
+            {
+                if (Convert.ToBoolean(rs["Upou"]))
+                {
+                    if(rs["MsgPIUp"] != null)
+                    {
+                        piSaida = new PI(true, MsgPIUp: (string)rs["MsgPIUp"]);
+                        retorno = true;
+                    }
+                }
+            }
+            conexao.Close();
+            return retorno;
+        }
+    }
+}
