918501abbf4c54a809ed9409dbd55ff4268cba51
Yummi <YummiSenpai@outlook.com>
2019-08-11 02:13:50 -0300

    começei a fazer o site yay

---
+++ Site/index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using MainDatabaseControler.DAO;
+using MainDatabaseControler.Modelos;
+using System;
+
+namespace Site
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btOk_Click1(object sender, EventArgs e)
+        {
+            Servidores servidor = new Servidores(Convert.ToUInt64(txId.Text));
+            if (new ServidoresDAO().GetPrefix(ref servidor))
+            {
+                lbPrefix.Text += new string(servidor.Prefix);
+            }
+            else
+            {
+                lbPrefix.Text = "não encontrei o prefixo desse servidor";
+            }
+        }
+    }
+}
