ce59b3f17aa40424e000245dfe20698a96ec2368
Lucas Mendes Campos <lucasmc2709@live.com>
2019-08-17 08:32:40 -0300

    NFSW rewrite

--- Bot/Comandos/Nsfw.cs
+++ Bot/Comandos/Nsfw.cs
@@ -1,5 +1,6 @@
 ﻿using Bot.Constantes;
 using Bot.Extensions;
+using Bot.GenericTypes;
 using Discord.Commands;
 using MainDatabaseControler.DAO;
 using MainDatabaseControler.Modelos;
@@ -9,17 +10,22 @@ using static MainDatabaseControler.Modelos.Servidores;
 
 namespace Bot.Comandos
 {
-    public class Nsfw : Image
+    public class Nsfw : GenericModule
     {
-        public void hentai(CommandContext context, object[] args)
+        public Nsfw(CommandContext contexto, object[] args) : base (contexto, args)
+        {
+
+        }
+
+        public void hentai()
         {
             Links links = new Links();
 
             List<Tuple<string, string>> imgs = new List<Tuple<string, string>>();
             imgs.Add(links.hentai);
-            if (!context.IsPrivate)
+            if (!contexto.IsPrivate)
             {
-                Servidores servidor = new Servidores(context.Guild.Id);
+                Servidores servidor = new Servidores(contexto.Guild.Id);
                 if(new ServidoresDAO().GetPermissoes(ref servidor))
                 {
                     if(servidor.Permissoes == PermissoesServidores.LolisEdition || servidor.Permissoes == PermissoesServidores.ServidorPika)
@@ -29,18 +35,18 @@ namespace Bot.Comandos
                     }
                 }
             }
-            new ImageExtensions().getImg(context, imgs: imgs.ToArray(), nsfw: true);
+            new ImageExtensions().getImg(contexto, imgs: imgs.ToArray(), nsfw: true);
         }
 
-        public void hentaibomb(CommandContext context, object[] args)
+        public void hentaibomb()
         {
             Links links = new Links();
 
             List<Tuple<string, string>> imgs = new List<Tuple<string, string>>();
             imgs.Add(links.hentai);
-            if (!context.IsPrivate)
+            if (!contexto.IsPrivate)
             {
-                Servidores servidor = new Servidores(context.Guild.Id);
+                Servidores servidor = new Servidores(contexto.Guild.Id);
                 if (new ServidoresDAO().GetPermissoes(ref servidor))
                 {
                     if (servidor.Permissoes == PermissoesServidores.LolisEdition || servidor.Permissoes == PermissoesServidores.ServidorPika)
@@ -50,14 +56,14 @@ namespace Bot.Comandos
                     }
                 }
             }
-            new ImageExtensions().getImg(context, img: links.hentai, nsfw: true, quantidade: 5);
+            new ImageExtensions().getImg(contexto, img: links.hentai, nsfw: true, quantidade: 5);
         }
 
-        public void anal(CommandContext context, object[] args)
+        public void anal()
         {
             Links links = new Links();
 
-            new ImageExtensions().getImg(context, img: links.anal, nsfw: true);
+            new ImageExtensions().getImg(contexto, img: links.anal, nsfw: true);
 
         }
     }
