b8f8886068ddbc0dca5febde39f1a2d8c7096262
Lucas Mendes Campos <lucasmc2709@live.com>
2019-08-26 15:45:44 -0300

    Documentado a classe Log

--- Bot/Nucleo/Log.cs
+++ Bot/Nucleo/Log.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace Bot.Nucleo
 {
+    //Classe responsavel por mandar o log da discord.net para a interface de usuario
     public class Log
     {
+        //Evento que captura o log da discord.net e joga para a interface de usuario
         public Task LogTask(LogMessage msg)
         {
             MethodInfo metodo = SingletonLogs.tipo.GetMethod("Log");
