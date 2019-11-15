ac45ecef83b06f96bd16413fd587f854ed5ec2a8
Lucas Mendes Campos <lucasmc2709@live.com>
2019-11-15 15:54:25 -0300

    Especiais portada com sucesso

---
+++ Bot/Comandos/Especiais.cs
@@ -0,0 +1,187 @@
+﻿using Bot.Extensions;
+using Bot.GenericTypes;
+using Discord;
+using Discord.Commands;
+using MainDatabaseControler.DAO;
+using MainDatabaseControler.Modelos;
+using System;
+using System.Threading.Tasks;
+using static MainDatabaseControler.Modelos.Servidores;
+using UserExtensions = Bot.Extensions.UserExtensions;
+
+namespace Bot.Comandos
+{
+    public class Especiais : GenericModule
+    {
+
+        public Especiais(CommandContext contexto, string prefixo, string[] comando) : base(contexto, prefixo, comando)
+        {
+
+        }
+
+        public async Task insult()
+        {
+            ulong id = 0;
+            if (!Contexto.IsPrivate)
+            {
+                id = Contexto.Guild.Id;
+            }
+
+            Servidores servidor = new Servidores(id, PrefixoServidor.ToCharArray());
+
+            Tuple<bool, Servidores> sucesso = await new ServidoresDAO().GetPermissoesAsync(servidor);
+            if (!Contexto.IsPrivate && sucesso.Item1 && servidor.Permissoes == PermissoesServidores.ServidorPika || (await new AdmsExtensions().GetAdm(new Usuarios(Contexto.User.Id))).Item1)
+            {
+                Insultos insulto = new Insultos();
+                Tuple<bool, Insultos> res = await new InsultosDAO().GetInsultoAsync(insulto);
+                insulto = res.Item2;
+                if (res.Item1)
+                {
+                    string[] comando = Comando;
+                    string msg = string.Join(" ", comando, 1, (comando.Length - 1));
+                    Tuple<IUser, string> user = new UserExtensions().GetUser(await Contexto.Guild.GetUsersAsync(), msg);
+
+                    if (user.Item1 != null)
+                    {
+                        await Contexto.Message.DeleteAsync();
+                        string author = "";
+                        string icon = "";
+                        try
+                        {
+                            IUser authorUser = await Contexto.Client.GetUserAsync(insulto.Usuario.Id);
+                            author = authorUser.ToString();
+                            icon = authorUser.GetAvatarUrl();
+                        }
+                        catch
+                        {
+                            author = insulto.Usuario.Nome;
+                        }
+
+                        await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithDescription($"{user.Item1.Mention} {insulto.Insulto}")
+                                .WithColor(Color.DarkPurple)
+                                .WithFooter(await StringCatch.GetString("insultCriado", "Insulto criado por: {0}", author), icon)
+                            .Build());
+                    }
+                    else
+                    {
+                        await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithTitle(await StringCatch.GetString("insultSemPessoa", "Você não me disse quem deve insultar"))
+                                .AddField(await StringCatch.GetString("usoCmd", "Uso do Comando:"), await StringCatch.GetString("usoInsult", "`{0}insult @pessoa`", PrefixoServidor))
+                                .AddField(await StringCatch.GetString("exemploCmd", "Exemplo:"), await StringCatch.GetString("usoInsult", "`{0}insult @Brunoow#7239`", PrefixoServidor))
+                                .WithColor(Color.Red)
+                            .Build());
+                    }
+                }
+                else
+                {
+                    await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                            .WithDescription(await StringCatch.GetString("insultFail", "**{0}** infelizmente ainda não tem nenhum insulto 😔", Contexto.User.Username))
+                            .WithColor(Color.Red)
+                        .Build());
+                }
+            }
+            else
+            {
+                await new Ajuda(Contexto, PrefixoServidor, Comando).MessageEventExceptions(new NullReferenceException(), servidor);
+            }
+        }
+
+        public async Task criarinsulto()
+        {
+            ulong id = 0;
+            if (!Contexto.IsPrivate)
+            {
+                id = Contexto.Guild.Id;
+            }
+
+            Servidores servidor = new Servidores(id, PrefixoServidor.ToCharArray());
+            Usuarios usuario = new Usuarios(Contexto.User.Id, Contexto.User.ToString());
+
+            Tuple<bool, Servidores> sucesso = await new ServidoresDAO().GetPermissoesAsync(servidor);
+            if (!Contexto.IsPrivate && sucesso.Item1 && servidor.Permissoes == PermissoesServidores.ServidorPika || (await new AdmsExtensions().GetAdm(new Usuarios(Contexto.User.Id))).Item1)
+            {
+                string[] comando = Comando;
+                string msg = string.Join(" ", comando, 1, (comando.Length - 1));
+
+                if (msg != "")
+                {
+                    Insultos insulto = new Insultos(msg, usuario);
+                    if (await new InsultosDAO().InserirInsultoAsync(insulto))
+                    {
+                        await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithDescription(await StringCatch.GetString("createinsultCriado", "**{0}** o insulto foi adicinado", Contexto.User.ToString()))
+                                .WithColor(Color.DarkPurple)
+                            .Build());
+                    }
+                }
+                else
+                {
+                    await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                        .WithTitle(await StringCatch.GetString("criarinsultoErro", "Você precisa me falar um insulto"))
+                        .AddField(await StringCatch.GetString("usoCmd", "Uso do Comando:"), await StringCatch.GetString("usoCriarinsulto", "`{0}criarinsulto insulto`", PrefixoServidor))
+                        .AddField(await StringCatch.GetString("exemploCmd", "Exemplo:"), await StringCatch.GetString("exemploCirarinsulto", "`{0}criarinsulto joguei uma pedra em você e ela entrou em orbita`", PrefixoServidor))
+                        .WithColor(Color.Red)
+                       .Build());
+                }
+            }
+            else
+            {
+                await new Ajuda(Contexto, PrefixoServidor, Comando).MessageEventExceptions(new NullReferenceException(), servidor);
+            }
+
+        }
+
+        public async Task fuckadd()
+        {
+            ulong id = 0;
+            if (!Contexto.IsPrivate)
+            {
+                id = Contexto.Guild.Id;
+            }
+
+            Servidores servidor = new Servidores(id, PrefixoServidor.ToCharArray());
+            Usuarios usuario = new Usuarios(Contexto.User.Id, Contexto.User.ToString());
+
+            Tuple<bool, Servidores> sucesso = await new ServidoresDAO().GetPermissoesAsync(servidor);
+            if (!Contexto.IsPrivate && sucesso.Item1 && servidor.Permissoes == PermissoesServidores.ServidorPika || (await new AdmsExtensions().GetAdm(new Usuarios(Contexto.User.Id))).Item1)
+            {
+                string[] comando = Comando;
+                try
+                {
+                    if (await new HttpExtensions().IsImageUrl(comando[1]))
+                    {
+                        bool _explicit = Convert.ToBoolean(comando[2]);
+                        Fuck fuck = new Fuck(_explicit, comando[1], usuario);
+                        await new FuckDAO().AddImgAsync(fuck);
+
+                        await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithDescription(await StringCatch.GetString("addFuckCriado", "**{0}** a imagem foi adicionada", Contexto.User.ToString()))
+                                .WithColor(Color.DarkPurple)
+                            .Build());
+                    }
+                    else
+                    {
+                        await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithDescription(await StringCatch.GetString("addFucknEhImg", "**{0}** isso n eh uma imagem meu caro", Contexto.User.ToString()))
+                                .WithColor(Color.Red)
+                            .Build());
+                    }
+                }
+                catch
+                {
+                    await Contexto.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                            .WithTitle(await StringCatch.GetString("fuckAddErro", "{0} Eh meu caro teve um erro na hora de adicionar a img", Contexto.User.ToString()))
+                            .AddField(await StringCatch.GetString("usoCmd", "Uso do Comando:"), await StringCatch.GetString("usoFuckAdd", "`{0}fuckadd <Url Img> <Explicit>`", PrefixoServidor))
+                            .AddField(await StringCatch.GetString("exemploCmd", "Exemplo: "), await StringCatch.GetString("usoFuckAdd", "`{0}fuckadd https://i.imgur.com/JDlJzBC.gif false`", PrefixoServidor))
+                            .WithColor(Color.Red)
+                        .Build());
+                }
+            }
+            else
+            {
+                await new Ajuda(Contexto, PrefixoServidor, Comando).MessageEventExceptions(new NullReferenceException(), servidor);
+            }
+        }
+    }
+}
