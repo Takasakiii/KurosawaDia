fe1ac55f0596c46e9a9a708d87c1d97bb0d6768b
Lucas Mendes Campos <lucasmc2709@live.com>
2019-11-20 19:58:52 -0300

    Atualização mais importante de todo bot sqn

---
+++ Bot/Singletons/InfoImportante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Singletons
+{
+    public static class InfoImportante
+    {   
+        public const string VersaoNumb = "1.2.3";
+        public const string VersaoName = "Começar Traço";
+
+        public const string conviteDia = "https://ayura.com.br/links/bot";
+        public const string conviteServer = "https://ayura.com.br/dia";
+    }
+}
