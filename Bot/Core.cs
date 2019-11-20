78d2c73a5beb093ff6b67bcc2eb3d4591c6238bf
Lucas Mendes Campos <lucasmc2709@live.com>
2019-11-20 16:08:51 -0300

    Core agr tem um retry para conexao perdida do discord (agr ela morre menos)

--- Bot/Core.cs
+++ Bot/Core.cs
@@ -5,6 +5,7 @@ using Bot.Singletons;
 using ConfigurationControler.DAO;
 using ConfigurationControler.Modelos;
 using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 
 namespace Bot
@@ -15,21 +16,34 @@ namespace Bot
     {
         public async Task CriarClienteAsync()
         {
-            SingletonClient.criarClient();
+            bool repetir = false;
+            do
+            {
+                try
+                {
+                    SingletonClient.criarClient();
 
-            DiaConfig config = await new DiaConfigDAO().CarregarAsync();
+                    DiaConfig config = await new DiaConfigDAO().CarregarAsync();
 
 
-            SingletonClient.client.MessageReceived += new MessageEvent(config, new ModulesConcat<GenericModule>()).MessageReceived;
-            SingletonClient.client.LoggedIn += new LogInEvent(config).LogIn;
-            SingletonClient.client.Log += new LogEvent().LogTask;
-            SingletonClient.client.ShardReady += new ReadyEvent().ShardReady;
-            SingletonClient.client.JoinedGuild += new JoinedGuildEvent().JoinedGuild;
-            SingletonClient.client.LeftGuild += new LeftGuildEvent().LeftGuild;
-            SingletonClient.client.UserJoined += new UserJoinedEvent().UserJoined;
-            SingletonClient.client.UserLeft += new UserLeftEvent().UserLeft;
+                    SingletonClient.client.MessageReceived += new MessageEvent(config, new ModulesConcat<GenericModule>()).MessageReceived;
+                    SingletonClient.client.LoggedIn += new LogInEvent(config).LogIn;
+                    SingletonClient.client.Log += new LogEvent().LogTask;
+                    SingletonClient.client.ShardReady += new ReadyEvent().ShardReady;
+                    SingletonClient.client.JoinedGuild += new JoinedGuildEvent().JoinedGuild;
+                    SingletonClient.client.LeftGuild += new LeftGuildEvent().LeftGuild;
+                    SingletonClient.client.UserJoined += new UserJoinedEvent().UserJoined;
+                    SingletonClient.client.UserLeft += new UserLeftEvent().UserLeft;
 
-            await IniciarAsync(config);
+                    await IniciarAsync(config);
+                    repetir = false;
+                }
+                catch (Exception e)
+                {
+                    repetir = true;
+                    await LogEmiter.EnviarLogAsync(e);
+                }
+            } while (repetir);
         }
 
         private async Task IniciarAsync(DiaConfig diaConfig)
