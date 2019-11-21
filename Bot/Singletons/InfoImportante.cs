047136c6db2189a9cd22b6646fae6e7076754e37
Lucas Mendes Campos <lucasmc2709@live.com>
2019-11-20 23:24:00 -0300

    Path1

--- Bot/Singletons/InfoImportante.cs
+++ Bot/Singletons/InfoImportante.cs
@@ -6,8 +6,8 @@ namespace Bot.Singletons
 {
     public static class InfoImportante
     {   
-        public const string VersaoNumb = "1.2.3";
-        public const string VersaoName = "Começar Traço";
+        public const string VersaoNumb = "1.2.3.1";
+        public const string VersaoName = "Começar Traço (Patch 01)";
 
         public const string conviteDia = "https://ayura.com.br/links/bot";
         public const string conviteServer = "https://ayura.com.br/dia";
