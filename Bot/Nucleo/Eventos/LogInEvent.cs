0cfe772afdb1487574552dfd0fd4fce6a9b31915
Yummi <YummiSenpai@outlook.com>
2019-11-23 12:39:28 -0300

    Mostrando o status again yay?

--- Bot/Nucleo/Eventos/LogInEvent.cs
+++ Bot/Nucleo/Eventos/LogInEvent.cs
@@ -29,37 +29,21 @@ namespace Bot.Nucleo.Eventos
                     {
                         try
                         {
-                            int users = 0;
-                            foreach (SocketGuild servidor in SingletonClient.client.Guilds)
-                            {
-                                users += servidor.Users.Count;
-                            }
-
-                            StringVarsControler varsControler = new StringVarsControler();
-                            VarTypes[] vars = new VarTypes[] {
-                                new VarTypes("%prefixo%", diaConfig.prefix),
-                                new VarTypes("%servidores%", SingletonClient.client.Guilds.Count.ToString()),
-                                new VarTypes("%usuarios%", users.ToString())
-                            };
-                            varsControler.AdicionarComplementos(vars);
-
                             switch (status[i].status_tipo)
                             {
                                 case Status.TiposDeStatus.Jogando:
-                                    await SingletonClient.client.SetGameAsync(varsControler.SubstituirVariaveis(status[i].status_jogo), status[i].status_url, Discord.ActivityType.Playing);
+                                    await SingletonClient.client.SetGameAsync(status[i].status_jogo, status[i].status_url, Discord.ActivityType.Playing);
                                     break;
                                 case Status.TiposDeStatus.Live:
-                                    await SingletonClient.client.SetGameAsync(varsControler.SubstituirVariaveis(status[i].status_jogo), status[i].status_url, Discord.ActivityType.Streaming);
+                                    await SingletonClient.client.SetGameAsync(status[i].status_jogo, status[i].status_url, Discord.ActivityType.Streaming);
                                     break;
                                 case Status.TiposDeStatus.Ouvindo:
-                                    await SingletonClient.client.SetGameAsync(varsControler.SubstituirVariaveis(status[i].status_jogo), status[i].status_url, Discord.ActivityType.Listening);
+                                    await SingletonClient.client.SetGameAsync(status[i].status_jogo, status[i].status_url, Discord.ActivityType.Listening);
                                     break;
                                 case Status.TiposDeStatus.Assistindo:
-                                    await SingletonClient.client.SetGameAsync(varsControler.SubstituirVariaveis(status[i].status_jogo), status[i].status_url, Discord.ActivityType.Watching);
+                                    await SingletonClient.client.SetGameAsync(status[i].status_jogo, status[i].status_url, Discord.ActivityType.Watching);
                                     break;
                             }
-                            varsControler = null;
-                            GC.Collect();
                         }
                         catch (Exception e)
                         {
