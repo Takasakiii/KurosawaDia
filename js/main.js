2af1915a58b49e1df63c36b2ec76cf05b05db17f
Takasakiii <lucasmc2709@live.com>
2019-12-14 13:52:51 -0300

    pagina inicial top

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function shibaGay(url){
+    
+}
+
+let btnServer = document.getElementById('btnServer');
+let btnConvite = document.createElement('btnConvite');
+
+btnServer.onclick = function(){
+    shibaGay('serverDia');
+}
+
+btnConvite.onclick = function(){
+    shibaGay('conviteDia');
+}
