961f0c7f558727ad06bccedc3d9cbf253924072f
Lucas Mendes Campos <lucasmc2709@live.com>
2020-02-03 02:20:06 -0300

    Alterando configurações da build

--- Bot/Constantes/InfoImportante.cs
+++ Bot/Constantes/InfoImportante.cs
@@ -2,8 +2,8 @@
 {
     public static class InfoImportante
     {   
-        public const string VersaoNumb = "1.2.5.1";
-        public const string VersaoName = "Nozomi eu estou com fome! - Patch01";
+        public const string VersaoNumb = "1.2.5.2";
+        public const string VersaoName = "Nozomi eu estou com fome! - Patch02";
 
         public const string conviteDia = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=conviteDia";
         public const string conviteServer = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=serverDia";
