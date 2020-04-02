d89a9e9fb18f6a68e98173b659fe6442fca24f35
FlameUnit <mrvulcandragonex@gmail.com>
2020-04-02 20:27:17 -0300

    Inicio da interface WPF

---
+++ Interfaces/WpfNetCore/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfNetCore.Views;
+
+namespace WpfNetCore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            Inicio inicio = new Inicio();
+            inicio.Show();
+        }
+    }
+}
---
+++ Interfaces/WpfNetCore/Views/Inicio.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ConfigController.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfNetCore.Views
+{
+    /// <summary>
+    /// Interaction logic for Inicio.xaml
+    /// </summary>
+    public partial class Inicio : Window
+    {
+        private BaseConfig Config { get; set; }
+
+        public Inicio()
+        {
+            InitializeComponent();
+            Initialized += Inicio_Initialized;
+        }
+
+        private async void Inicio_Initialized(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
