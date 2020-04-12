50aa00f79624467a5f758a817cf84b24182303cf
Lucas Mendes Campos <lucasmc2709@live.com>
2020-04-12 00:31:34 -0300

    DBSYNC

--- Core/DataBaseController/SQLs/CustomReactions.sql
+++ Core/DataBaseController/SQLs/CustomReactions.sql
@@ -4,5 +4,5 @@ create procedure AddCR (
 	in _modo bool,
 	in _servidor bigint
 ) begin
-	insert into CustomReactions (modo_cr, resposta_cr, servidor_cr, trigger_cr) values (_modo, _resposta, _modo, (select GetCodServidor(_servidor )), _t );
+	insert into CustomReactions (modo_cr, resposta_cr, servidor_cr, trigger_cr) values (_modo, _resposta, (select GetCodServidor(_servidor )), _t );
 end;
