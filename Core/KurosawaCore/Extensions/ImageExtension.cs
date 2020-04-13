fc8c695016cd09658fb862ff45c249b7bfa509ab
FlameUnit <mrvulcandragonex@gmail.com>
2020-04-13 18:57:40 -0300

    Comandos de imagem

---
+++ Core/KurosawaCore/Extensions/ImageExtension.cs
@@ -0,0 +1,89 @@
+﻿using KurosawaCore.Extensions.HttpExtension;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurosawaCore.Extensions
+{
+    internal class ImageExtension
+    {
+        private const string urlBaseNekosLife = "https://nekos.life/api/v2/img/";
+        private const string urlBaseRandomDog = "https://random.dog/woof.json";
+        private const string urlBaseLolisLife = "https://api.lolis.life/random";
+
+        private enum NekosLifeType
+        {
+            neko,
+            meow
+        }
+
+        internal async Task<string> GetCat()
+        {
+            string url;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage httpResponseMessage = await client.GetAsync($"{urlBaseNekosLife}{(NekosLifeType)new Random().Next(2)}");
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    string json = await httpResponseMessage.Content.ReadAsStringAsync();
+                    url = JsonConvert.DeserializeObject<NekosLife>(json).Url;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+
+            return url;
+        }
+
+        internal async Task<string> GetDog()
+        {
+            string url;
+
+            do
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await client.GetAsync(urlBaseRandomDog);
+                    if (httpResponseMessage.IsSuccessStatusCode)
+                    {
+                        string json = await httpResponseMessage.Content.ReadAsStringAsync();
+                        url = JsonConvert.DeserializeObject<RandomDog>(json).Url;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+            } while (url.Contains(".mp4"));
+
+            return url;
+        }
+
+        internal async Task<string> GetLoli()
+        {
+            string url;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage httpResponseMessage = await client.GetAsync(urlBaseLolisLife);
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    string json = await httpResponseMessage.Content.ReadAsStringAsync();
+                    url = JsonConvert.DeserializeObject<LolisLife>(json).Url;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+
+            return url;
+        }
+    }
+}
---
+++ Core/KurosawaCore/Modulos/Image.cs
@@ -0,0 +1,79 @@
+﻿using DataBaseController.Abstractions;
+using DataBaseController.DAOs;
+using DataBaseController.Modelos;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using KurosawaCore.Extensions;
+using KurosawaCore.Models.Atributes;
+using System;
+using System.Threading.Tasks;
+
+namespace KurosawaCore.Modulos
+{
+    [Modulo("Image", "🖼")]
+    [Description("Este módulo possui imagens fofinhas para agraciar seu computador.")]
+    public class Image
+    {
+        [Command("cat")]
+        [Description("Nyann")]
+        public async Task Cat(CommandContext ctx)
+        {
+            string url = await new ImageExtension().GetCat();
+            await ctx.RespondAsync(embed: new DiscordEmbedBuilder
+            {
+                Title = "Meow",
+                Description = $"[Link Direto]({url})",
+                ImageUrl = url,
+                Color = DiscordColor.Turquoise
+            });
+        }
+
+        [Command("dog")]
+        [Description("LuckShiba")]
+        public async Task Dog(CommandContext ctx)
+        {
+            Random random = new Random();
+
+            if (random.Next(100) != 24)
+            {
+                string url = await new ImageExtension().GetDog();
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder
+                {
+                    Title = "Woof",
+                    Description = $"[Link Direto]({url})",
+                    ImageUrl = url,
+                    Color = DiscordColor.Turquoise
+                });
+            }
+            else
+            {
+                DiscordUser user = await ctx.Client.GetUserAsync(355750436424384524);
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder
+                {
+                    Title = "Se ta procurando um cachorinho ? Me adote",
+                    Description = $"Me adote no discord **{user.Username}#{user.Discriminator}** Woof Woof",
+                    ThumbnailUrl = user.AvatarUrl,
+                    Color = DiscordColor.Turquoise
+                });
+            }
+        }
+
+        [Command("loli")]
+        [Description("Manda uma imagem para que vc seja preso")]
+        public async Task Loli(CommandContext ctx)
+        {
+            if ((byte)(await new ServidoresDAO().Get(new Servidores { ID = ctx.Guild.Id })).Espercial <= (byte)TiposServidores.LolisEdition)
+                throw new Exception();
+
+            string url = await new ImageExtension().GetLoli();
+            await ctx.RespondAsync(embed: new DiscordEmbedBuilder 
+            {
+                Title = "S-Senpai",
+                Description = $"[Link Direto]({url})",
+                ImageUrl = url,
+                Color = DiscordColor.Turquoise
+            });
+        }
+    }
+}
