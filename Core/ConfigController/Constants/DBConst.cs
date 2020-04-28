e1663165f6abd08f9337a440112b8e8f48a91936
Lucas Mendes Campos <lucasmc2709@live.com>
2020-04-28 18:10:47 -0300

    Signed-off-by: Lucas Mendes Campos <lucasmc2709@live.com>

--- Core/ConfigController/Constants/DBConst.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConfigController.Constants
-{
-    internal sealed class DBConst
-    {
-        internal const string ConfigName = "Config.dia";
-    }
-}
--- Core/DataBaseController/Abstractions/TiposCanais.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataBaseController.Abstractions
-{
-    public enum TiposCanais : byte
-    {
-        Default = 0,
-        BemVindo = 1,
-        Sair = 2
-    }
-}
