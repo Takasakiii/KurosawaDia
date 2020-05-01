457e596036c66bb9a377e28a160bdbb106aab146
Lucas Mendes Campos <lucasmc2709@live.com>
2020-05-01 16:53:52 -0300

    Paginator test

---
+++ Core/KurosawaCore/Extensions/PagesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KurosawaCore.Extensions
+{
+    internal class PagesExtensions
+    {
+        internal List<Page> Paginador { get; private set; }
+        internal PagesExtensions(IEnumerable<Page> paginador)
+        {
+            Paginador = paginador.ToList();
+        }
+
+        internal PagesExtensions()
+        {
+            Paginador = new List<Page>();
+        }
+
+        internal void AdicionarPaginaString(string content)
+        {
+            Paginador.Add(new Page
+            {
+                Content = content
+            });
+        }
+
+        internal void AdicionarEmbed(DiscordEmbed embed)
+        {
+            Paginador.Add(new Page
+            {
+                Embed = embed
+            });
+        }
+
+        internal void AdicionarPage(Page pagina)
+        {
+            Paginador.Add(pagina);
+        }
+    }
+}
---
+++ Core/KurosawaCore/Modulos/Teste.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using KurosawaCore.Extensions;
+using KurosawaCore.Models.Atributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurosawaCore.Modulos
+{
+    [Modulo("Teste Modulo")]
+    [Description("Parte de teste e instavel, use por sua conta em risco")]
+    [Hidden]
+    public class Teste
+    {
+        [Command("page")]
+        [Description("testa paginators")]
+        public async Task PaginatorTest(CommandContext ctx)
+        {
+            InteractivityModule interativy = ctx.Client.GetInteractivityModule();
+
+            PagesExtensions pgs = new PagesExtensions();
+            pgs.AdicionarPaginaString("pg 1");
+            pgs.AdicionarPaginaString("pg 2");
+            pgs.AdicionarPaginaString("pg 3");
+
+            // send the paginator
+            await interativy.SendPaginatedMessage(ctx.Channel, ctx.User, pgs.Paginador, TimeSpan.FromMinutes(5), TimeoutBehaviour.Delete);
+        }
+    }
+}
