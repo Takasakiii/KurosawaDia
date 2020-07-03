69b0d3b19bf9be9d1d8de32f2fe7de7eb6d5fe92
Takasakiii <lucasmc2709@live.com>
2020-07-03 16:39:04 -0300

    Atualizando a versão

--- Core/KurosawaCore/Constants/InfoImportante.cs
+++ Core/KurosawaCore/Constants/InfoImportante.cs
@@ -2,8 +2,8 @@
 {
     internal static class InfoImportante
     {
-        internal const string VersaoNumb = "1.3.1";
-        internal const string VersaoName = "New Horizons - Patch 1";
+        internal const string VersaoNumb = "1.3.2";
+        internal const string VersaoName = "New Horizons - Patch 2";
 
         internal const string conviteDia = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=conviteDia";
         internal const string conviteServer = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=serverDia";
