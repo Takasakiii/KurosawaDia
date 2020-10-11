73720d409bae405e3ae68597de357e68c611bf05
Gabriel Paulucci <mrvulcandragonex@gmail.com>
2020-10-11 18:57:11 -0300

    Validando se o token existe

--- src/bot.ts
+++ src/bot.ts
@@ -1,21 +1,24 @@
 import { Client } from 'discord.js'
 
 export default class KurosawaDia {
-
     private client: Client
     private token: string
 
     constructor(token: string) {
+        if (token === undefined) {
+            console.log('token não foi definido')
+            process.exit()
+        }
 
         this.client = new Client()
         this.token = token
-
     }
 
     start() : void {
+        this.client.on('ready', () => {
+            console.log('bot iniciado')
+        })
 
         this.client.login(this.token)
-
     }
-
 }
