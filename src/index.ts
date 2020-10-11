fcdf5eecd4ee99e6eb03b729bab8c8c43f372db6
Gabriel Paulucci <mrvulcandragonex@gmail.com>
2020-10-11 02:17:10 -0300

    Bot iniciando

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('teste').a
+import { Client } from 'discord.js'
+import config from './config/bot.json'
+
+const bot = new Client()
+
+bot.on('ready', () => {
+    console.log('bot iniciado')
+})
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.channel.send('pong')
+    }
+})
+
+bot.login(config.token)
