b5069aeeae84d37864c133f1d628f2b2b3cf0b39
Gabriel Paulucci <gabrielpauluccimunizmendes@gmail.com>
2020-10-27 21:13:38 -0300

    Add modules description and package test

---
+++ src/i18n/index.ts
@@ -0,0 +1,10 @@
+import i18n from 'i18n'
+
+i18n.configure({
+    locales: [
+        'pt-br'
+    ],
+    defaultLocale: 'pt-br',
+    directory: './src/i18n/locales',
+    objectNotation: true
+})
