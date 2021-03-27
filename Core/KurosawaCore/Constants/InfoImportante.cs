cdb595289f4b5ad794c11cfb8d60876c378ffd12
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-03-27 14:44:44 -0300

    Adicionado o zura list

--- Core/KurosawaCore/Constants/InfoImportante.cs
+++ Core/KurosawaCore/Constants/InfoImportante.cs
@@ -8,5 +8,6 @@
         internal const string conviteDia = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=conviteDia";
         internal const string conviteServer = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=serverDia";
         internal const string topgg = "https://kurosawa.zuraaa.com/paginas/redirecionar.html?pg=topgg";
+        internal const string zuraaaList = "https://www.zuraaa.com/bots/389917977862078484/";
     }
 }
