f993d46b5575bf7d028e599cc9dadf00d2d77099
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-04-27 22:47:50 -0300

    Fizalizando o comando de avatar

--- src/commands/util.rs
+++ src/commands/util.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use serenity::{builder::CreateEmbed, client::Context, framework::standard::{CommandResult, macros::{command, group}}, model::channel::Message};
 
 use crate::utils::user::get_user_from_id_or_mention;
@@ -21,8 +22,33 @@ pub async fn avatar(ctx: &Context, msg: &Message) -> CommandResult {
 
     let mut embed = CreateEmbed::default();
     embed.color(0x5fad47);
-    embed.title("Uiui que gatinho");
-    embed.image(avatar);
+    embed.image(&avatar);
+
+    let kurosawa = ctx.cache.current_user().await;
+
+    if user.id == kurosawa.id {
+        let titles = [
+            "Ownt, que amor, você realmente quer me ver 😍", 
+            "Assim você me deixa sem jeito 😊"
+        ];
+
+        let mut rng = rand::thread_rng();
+
+        embed.title(titles[rng.gen_range(0..titles.len())]);
+    } else {
+        let titles = [
+            "Nossa, que avatar bonito! Agora sei porque você queria vê-lo 🤣", 
+            "Vocês são realmente criativos para avatares 😂",
+            "Com um avatar assim seria um desperdício não se tornar uma idol 😃",
+            "Talvez se você colocasse um filtro ficaria melhor... 🤐"
+        ];
+
+        let mut rng = rand::thread_rng();
+
+        embed.title(titles[rng.gen_range(0..titles.len())]);
+    }
+
+    embed.description(format!("{}\n[Link direto]({})", user.tag(), &avatar));
 
     msg.channel_id.send_message(ctx, |x| x
         .set_embed(embed)
