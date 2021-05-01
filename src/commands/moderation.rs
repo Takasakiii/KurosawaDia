2c867974fffcde9aec06ca4f85a427763fbf4a94
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-04-30 22:53:58 -0300

    Limpando as mensagens dos usuarios

--- src/commands/moderation.rs
+++ src/commands/moderation.rs
@@ -13,40 +13,77 @@ pub struct Moderation;
 #[max_args(2)]
 async fn limpar_chat(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let mut count = args.single::<u64>().unwrap_or(10);
-    count += 1;
 
     match get_user_from_args(ctx, &mut args).await {
         Some(user) => {
+            let mut message_ref = msg.id;
 
-        },
-        None => {
-            'send_messages: loop {
+            'delete_messages_user: loop {
+
                 let count_messages = if count == 0 {
-                    break 'send_messages;
+                    break 'delete_messages_user
                 } else if count <= 100 {
-                    let temp = count;
-                    count -= count;
-                    temp
+                    count
                 } else {
-                    count -= 100;
                     100
                 };
 
                 let messages = msg.channel_id.messages(ctx, |x| x
                     .limit(count_messages)
+                    .before(message_ref)
                 ).await;
 
                 match messages {
                     Ok(messages) => {
-                        msg.channel_id.delete_messages(ctx, messages).await?;
+                        message_ref = messages.last().unwrap().id;
+
+                        let messages = messages
+                            .iter()
+                            .filter(|x| x.author.id == user.id)
+                            .collect::<Vec<&Message>>();
+
+                        match msg.channel_id.delete_messages(ctx, &messages).await {
+                            Ok(_) => count -= messages.len() as u64,
+                            Err(_) => break 'delete_messages_user
+                        }
                     },
-                    Err(_) => {
-                        break 'send_messages;
-                    }
+                    Err(_) => break 'delete_messages_user
+                }
+            }
+        },
+        None => {
+            let mut message_ref = msg.id;
+
+            'delete_messages: loop {
+                let count_messages = if count == 0 {
+                    break 'delete_messages
+                } else if count <= 100 {
+                    count
+                } else {
+                    100
+                };
+
+                let messages = msg.channel_id.messages(ctx, |x| x
+                    .limit(count_messages)
+                    .before(message_ref)
+                ).await;
+
+                match messages {
+                    Ok(messages) => {
+                        message_ref = messages.last().unwrap().id;
+
+                        match msg.channel_id.delete_messages(ctx, &messages).await {
+                            Ok(_) => count -= messages.len() as u64,
+                            Err(_) => break 'delete_messages
+                        }
+                    },
+                    Err(_) => break 'delete_messages
                 }
             }
         }
     }
+
+    msg.delete(ctx).await?;
 
     Ok(())
 }
--- src/utils/user.rs
+++ src/utils/user.rs
@@ -6,7 +6,7 @@ pub async fn get_user_from_args(ctx: &Context, args: &mut Args) -> Option<User> 
         Err(_) => return None
     };
 
-    let user = match parse_username(&id) {
+    let user_id = match parse_username(&id) {
         Some(id) => id,
         None => match id.parse::<u64>() {
             Ok(id) => id,
@@ -14,5 +14,8 @@ pub async fn get_user_from_args(ctx: &Context, args: &mut Args) -> Option<User> 
         }
     };
 
-    ctx.cache.user(user).await
+    match ctx.cache.user(user_id).await {
+        Some(user) => Some(user),
+        None => ctx.http.get_user(user_id).await.ok()
+    }
 }
