955563268d969bd78397bf6d9d136737c02b3b73
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-05-11 19:03:32 -0300

    Comando furrar finalizado

--- src/commands/weeb.rs
+++ src/commands/weeb.rs
@@ -2,6 +2,8 @@ use rand::{Rng, thread_rng};
 use serenity::{builder::CreateEmbed, client::Context, framework::standard::{Args, CommandResult, macros::{command, group}}, model::channel::Message};
 use unidecode::unidecode_char;
 
+use crate::utils::constants::colors;
+
 #[group]
 #[commands(owoify)]
 pub struct Weeb;
@@ -11,7 +13,7 @@ pub struct Weeb;
 #[min_args(1)]
 async fn owoify(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let text = args.remains().unwrap();
-    let mut chars = text.chars();
+    let chars = text.chars().collect::<Vec<char>>();
     
     if text.len() > 800 {
         return Err("Texto maior que 800".into());
@@ -19,47 +21,60 @@ async fn owoify(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
     let mut result = "".to_string();
 
-    let faces = ["OwO", "owo", "oωo", "òωó", "°ω°", "UwU", ">w<", "^w^"];
+    let faces = [" OwO ", " owo ", " oωo ", " òωó ", " °ω° ", " UwU ", " >w< ", " ^w^ "];
     
-    let mut rng = thread_rng();
-
     let mut indexer = 0;
-
+    
     loop {
-        match chars.next() {
-            Some(ch) => {
-                if ch == 'r' || ch == 'l' {
-                    result.push('w');
-                } else if text.len() - indexer != 1 && (ch == 'n' || ch == 'N') {
-                    let next = chars.nth(indexer + 1).unwrap();
-                    let next = unidecode_char(next).chars().next().unwrap().to_lowercase();
+        if chars.len() <= indexer {
+            break;
+        }
 
-                    
-                } else if ch == 'R' || ch == 'L' {
-                    result.push('w');
-                } else if ch == '!' {
-                    if indexer != 0 && chars.nth(indexer - 1).unwrap() == '@' {
-                        result.push(ch);
-                    } else if !(text.len() - indexer != 1 && chars.nth(indexer + 1).unwrap() == '!') {
-                        result.push_str(faces[rng.gen_range(0..faces.len())]);
-                    }
-                } else if text.len() - indexer > 2 {
-                    if ch == 'o' && chars.nth(indexer + 1).unwrap() == 'v' && chars.nth(indexer + 2).unwrap() == 'e' {
-                        result.push_str("uv");
-                        indexer += 1;
-                    } else if ch == 'O' && chars.nth(indexer + 1).unwrap() == 'V' && chars.nth(indexer + 2).unwrap() == 'E'  {
-                        result.push_str("UV");
-                        indexer += 1;
-                    } else {
-                        result.push(ch);
-                    }
+        let ch = chars[indexer];
+
+        if ch == 'r' || ch == 'l' {
+            result.push('w');
+        } else if text.len() - indexer != 1 && (ch == 'n' || ch == 'N') {
+            let next = chars[indexer + 1];
+            let next_norm = unidecode_char(next)
+                .chars()
+                .next()
+                .unwrap();
+            let next_lower = next_norm
+                .to_lowercase()
+                .next()
+                .unwrap();
+
+            if next_lower == 'a' || next_lower == 'e' || next_lower == 'i' || next_lower == 'o' || next_lower == 'u' {
+                if next_norm == next_lower {
+                    result.push_str(format!("{}y", ch).as_str());
                 } else {
-                    result.push(ch);
+                    result.push_str(format!("{}Y", ch).as_str());
                 }
-            },
-            None => {
-                break;
+            } else {
+                result.push(ch);
             }
+        } else if ch == 'R' || ch == 'L' {
+            result.push('W');
+        } else if ch == '!' {
+            if indexer != 0 && chars[indexer - 1] == '@' {
+                result.push(ch);
+            } else if !(text.len() - indexer != 1 && chars[indexer + 1] == '!') {
+                let mut rng = thread_rng();
+                result.push_str(faces[rng.gen_range(0..faces.len())]);
+            }
+        } else if text.len() - indexer > 2 {
+            if ch == 'o' && chars[indexer + 1] == 'v' && chars[indexer + 2] == 'e' {
+                result.push_str("uv");
+                indexer += 2;
+            } else if ch == 'O' && chars[indexer + 1] == 'V' && chars[indexer + 2] == 'E'  {
+                result.push_str("UV");
+                indexer += 2;
+            } else {
+                result.push(ch);
+            }
+        } else {
+            result.push(ch);
         }
 
         indexer += 1;
@@ -67,11 +82,12 @@ async fn owoify(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
     let mut embed = CreateEmbed::default();
     embed.description(result);
+    embed.color(colors::PINK);
 
     msg.channel_id.send_message(ctx, |x| x
         .set_embed(embed)
         .reference_message(msg)
-    );
+    ).await?;
 
     Ok(())
 }
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,4 +1,5 @@
 pub mod colors {
     pub const GREEN: i32 = 0x5fad47;
     pub const PURPLE: i32 = 0xad3cc3;
+    pub const PINK: i32 = 0xff69b4;
 }
