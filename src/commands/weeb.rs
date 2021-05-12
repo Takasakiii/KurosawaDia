b018dc1d16d1a27e7ae63a2d2f69be7b68b5be0b
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-05-12 00:14:35 -0300

    Comando megumin

--- src/commands/weeb.rs
+++ src/commands/weeb.rs
@@ -5,7 +5,7 @@ use unidecode::unidecode_char;
 use crate::{apis::get_weeb_api, utils::{constants::colors, user::get_user_from_args}};
 
 #[group]
-#[commands(owoify, hug, kiss, slap, punch, lick, cry, pat, dance)]
+#[commands(owoify, hug, kiss, slap, punch, lick, cry, pat, dance, megumin)]
 pub struct Weeb;
 
 #[command("hug")]
@@ -223,6 +223,26 @@ async fn dance(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     };
 
     embed.title(title);
+
+    msg.channel_id.send_message(ctx, |x| x
+        .set_embed(embed)
+        .reference_message(msg)
+    ).await?;
+    
+    Ok(())
+}
+
+#[command("megumin")]
+#[only_in("guilds")]
+#[max_args(0)]
+async fn megumin(ctx: &Context, msg: &Message) -> CommandResult {
+    let api = get_weeb_api();
+    let image = api.get_random("megumin").await?;
+
+    let mut embed = CreateEmbed::default();
+    embed.image(image.url);
+    embed.color(colors::PINK);
+    embed.title("Megumin ❤");
 
     msg.channel_id.send_message(ctx, |x| x
         .set_embed(embed)
