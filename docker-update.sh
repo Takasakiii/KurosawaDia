9ae83d2420a1757b6f58ba6de5d5b03507ffaa77
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-06-16 01:08:49 -0300

    Parando o docker antes de deletar

--- docker-update.sh
+++ docker-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+docker stop kurosawa-bot
 docker rm kurosawa-bot
 docker pull ghcr.io/fardragi/kurosawadia:latest
 docker create --name kurosawa-bot --env-file .env --network Kurosawa ghcr.io/fardragi/kurosawadia
-docker run kurosawa-bot
+docker start kurosawa-bot
